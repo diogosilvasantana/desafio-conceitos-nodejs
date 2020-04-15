2ee76134001c575009948d97003bcb0333fb8be1
Diogo Silva <diogosilvaoficial@gmail.com>
2020-04-15 13:30:19 -0300

    Entrega Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulator, transaction) => {
+      return transaction.type === 'income'
+        ? acumulator + transaction.value
+        : acumulator;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acumulator, transaction) => {
+      return transaction.type === 'outcome'
+        ? acumulator + transaction.value
+        : acumulator;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
